a42458841c7e0584f4d89b5ead0299a85ca86904
Minh Anh <minhanh.itqn@gmail.com>
2019-08-09 13:21:17 +0700

    solustion 10 first question

--- less1/solutions.sql
+++ less1/solutions.sql
@@ -3,3 +3,7 @@
 
 -- 1.1 E.g Select the names of all the products in the store.
 select Name from Products;
+-- 1.2 Select the names and the prices of all the products in the store.
+SELECT `Name`,`Price` FROM `products`
+1.3 Select the name of the products with a price less than or equal to $200.
+SELECT `Name` FROM `products` WHERE `Price` >= 200
